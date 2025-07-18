405643a1fe1d5ac354249e574ed7ad0d99136a99
Hendrik Ebbers <hendrik.ebbers@web.de>
2025-07-18 09:28:05 +0200

    module system works

--- proto/src/main/java/com/hedera/hashgraph/sdk/proto/Foo.java
+++
@@ -1,4 +0,0 @@
-package com.hedera.hashgraph.sdk.proto;
-
-public class Foo {
-}
---
+++ proto/src/main/java/com/hedera/hashgraph/sdk/proto/Placeholder.java
@@ -0,0 +1,7 @@
+package com.hedera.hashgraph.sdk.proto;
+
+/**
+ * If the package does not exist IntelliJ shows an error in the module-info.java file.
+ */
+public class Placeholder {
+}
